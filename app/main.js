b395e677cf7ac6445d046f3709930a35e1f26157
Jawad Khawaja <jawkha.codes@gmail.com>
2024-06-06 12:08:13 +0500

    responding to request

--- app/main.js
+++ app/main.js
@@ -11,4 +11,9 @@ const server = net.createServer((socket) => {
 	})
 })
 
+server.on('connection', (socket) => {
+	socket.write('HTTP/1.1 200 OK\r\n\r\n')
+	socket.end()
+})
+
 server.listen(4221, 'localhost')
