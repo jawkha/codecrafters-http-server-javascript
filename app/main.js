e2e8c0eac920120f37ed9585cd579cba101a0b6a
Jawad Khawaja <jawkha.codes@gmail.com>
2024-06-07 15:24:20 +0500

    parse command line args for file storage path

--- app/main.js
+++ app/main.js
@@ -1,5 +1,12 @@
 const net = require('net')
 const fs = require('fs')
+const process = require('process')
+
+let fileStorage = './files/'
+
+if (process.argv.length > 3) {
+	fileStorage = process.argv[3]
+}
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log('Logs from your program will appear here!')
@@ -25,11 +32,11 @@ function createResponse({ verb, resource, protocol, headers }) {
 	} else if (resource.startsWith('/files')) {
 		const fileName = resource.split('/')[2]
 		if (fileName) {
-			const fileExists = fs.existsSync(`/tmp/${fileName}`)
+			const fileExists = fs.existsSync(`${fileStorage}${fileName}`)
 			if (!fileExists) {
 				return `HTTP/1.1 404 Not Found\r\n\r\n`
 			} else {
-				const file = fs.readFileSync(`/tmp/${fileName}`)
+				const file = fs.readFileSync(`${fileStorage}${fileName}`)
 				const headers = `application/octet-stream\r\nContent-Length: ${file.length}\r\n`
 				return `HTTP/1.1 200 OK\r\n${headers}\r\n${file}`
 			}
